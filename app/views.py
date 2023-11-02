2d8d85bfeb937551f8608085ec380abe54252058
Serhii Chukin <serjio2003@ukr.net>
2023-11-03 00:27:10 +0200

    feet: add search

--- app/views.py
+++ app/views.py
@@ -18,29 +18,31 @@ class FilmList(ListAPIView):
             if not fullmatch(r"-?release_year", ordering):
                 message = "Wrong ordering, try (-)release_year"
                 raise Http404(message)
-
             queryset = queryset.order_by(ordering)
 
         if film_type := GET.get('type'):
             if not film_type.isdigit():
                 message = "Wrong type, you should use integer"
                 raise Http404(message)
-
             queryset = queryset.filter(film_type=film_type)
 
         if category := GET.get('category'):
             if not category.isdigit():
                 message = "Wrong category, you should use integer"
                 raise Http404(message)
-
             queryset = queryset.filter(category=category)
 
         if genre := GET.get('genre'):
             if not genre.isdigit():
                 message = "Wrong genre, you should use integer"
                 raise Http404(message)
-
             queryset = queryset.filter(genere__id__contains=genre)
+
+        if not (search := GET.get('search')) is None:
+            if not search:
+                message = "Wrong search query, search query mastn't be empty"
+                raise Http404(message)
+            queryset = queryset.filter(name__contains=search)
 
         return queryset
 
