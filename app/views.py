e41c987755e0c2dda132c24426446ea031558a3d
Serhii Chukin <serjio2003@ukr.net>
2023-11-02 23:09:49 +0200

    feet: add sorting by released year

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from re import fullmatch
 
-# Create your views here.
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from app.models import Film
+from app.serializers import FilmDetailSerializer, FilmSerializer
+
+
+class FilmList(ListAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+
+    def get_queryset(self):
+        ordering = self.request.GET.get('ordering')
+
+        if fullmatch(r"-?release_year", ordering):
+            self.queryset = self.queryset.order_by(ordering)
+
+        return self.queryset
+
+
+class FilmDetail(RetrieveAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmDetailSerializer
+
