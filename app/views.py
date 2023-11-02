166a7c10147ad9c653d81bdf17a76f5e16639b89
Serhii Chukin <serjio2003@ukr.net>
2023-11-03 00:11:20 +0200

    feet: add rasin 404 if wwron parametres

--- app/views.py
+++ app/views.py
@@ -1,21 +1,48 @@
 from re import fullmatch
 
+from django.http import Http404
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+
 from app.models import Film
 from app.serializers import FilmDetailSerializer, FilmSerializer
 
 
 class FilmList(ListAPIView):
-    queryset = Film.objects.all()
     serializer_class = FilmSerializer
 
     def get_queryset(self):
-        ordering = self.request.GET.get('ordering')
+        queryset = Film.objects.all()
+        GET = self.request.GET
 
-        if fullmatch(r"-?release_year", ordering):
-            self.queryset = self.queryset.order_by(ordering)
+        if ordering := GET.get('ordering'):
+            if not fullmatch(r"-?release_year", ordering):
+                message = "Wrong ordering, try (-)release_year"
+                raise Http404(message)
 
-        return self.queryset
+            queryset = queryset.order_by(ordering)
+
+        if film_type := GET.get('type'):
+            if not film_type.isdigit():
+                message = "Wrong type, you should use integer"
+                raise Http404(message)
+
+            queryset = queryset.filter(film_type=film_type)
+
+        if category := GET.get('category'):
+            if not category.isdigit():
+                message = "Wrong category, you should use integer"
+                raise Http404(message)
+
+            queryset = queryset.filter(category=category)
+
+        if genre := GET.get('genre'):
+            if not genre.isdigit():
+                message = "Wrong genre, you should use integer"
+                raise Http404(message)
+
+            queryset = queryset.filter(genere__id__contains=genre)
+
+        return queryset
 
 
 class FilmDetail(RetrieveAPIView):
