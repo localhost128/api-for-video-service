a12c5ccaad5ec22729cee7d17960bc63298d642b
Serhii Chukin <serjio2003@ukr.net>
2023-11-02 22:02:07 +0200

    feet: add str methods; add ordering to Film model

--- app/models.py
+++ app/models.py
@@ -5,17 +5,29 @@ from django.core.validators import MinValueValidator
 class Type(models.Model):
     name = models.CharField(unique=True, max_length=50)
 
+    def  __str__(self):
+        return self.name
+
 
 class Genre(models.Model):
     name = models.CharField(unique=True, max_length=50)
+
+    def  __str__(self):
+        return self.name
 
 
 class Audio(models.Model):
     name = models.CharField(unique=True, max_length=50)
 
+    def  __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(unique=True, max_length=3)
+
+    def  __str__(self):
+        return self.name
 
 
 class Image(models.Model):
@@ -24,12 +36,20 @@ class Image(models.Model):
     film = models.ForeignKey(
             "Film", related_name="images", on_delete=models.CASCADE)
 
+    def  __str__(self):
+        return self.url
+
 
 class Film(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True, max_length=350)
     release_year = models.IntegerField(validators=[MinValueValidator(1900)])
     film_type = models.ForeignKey("Type", on_delete=models.PROTECT)
-    genere = models.ManyToManyField("Genre")
+    genere = models.ManyToManyField("Genre", blank=True)
     category = models.ForeignKey("Category", on_delete=models.PROTECT)
+
+    get_latest_by = ["name", "-release_year"]
+
+    def  __str__(self):
+        return self.name
 
