fbb5f048e580035131ff6c739936ed4b62010af4
Serhii Chukin <serjio2003@ukr.net>
2023-11-05 20:17:43 +0200

    feet: change settings to postgres db

--- video_service/settings.py
+++ video_service/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'video_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
