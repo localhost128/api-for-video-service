5c08215b818ab2cfca137b01ccd50bfdd3396e5f
Serhii Chukin <serjio2003@ukr.net>
2023-11-06 12:32:34 +0200

    refactor: refactor serializators

--- app/serializers.py
+++ app/serializers.py
@@ -3,36 +3,31 @@ from rest_framework import serializers
 from .models import Film, Image
 
 
-class FilmSerializer(serializers.ModelSerializer):
-    img = serializers.SerializerMethodField()
-    film_type = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    category = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    genres = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
+class FilmDetailSerializer(serializers.ModelSerializer):
+    images = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='url')
+
+    film_type = serializers.SlugRelatedField(
+        read_only=True, slug_field='name')
+
+    category = serializers.SlugRelatedField(
+        read_only=True, slug_field='name')
+
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='name')
+
+    class Meta:
+        model = Film
+        fields = ['id', 'name', 'description', 'release_year',
+                  'film_type', 'genre', 'category', 'images']
 
 
+class FilmSerializer(FilmDetailSerializer):
+    images = serializers.SerializerMethodField()
 
-    def get_img(self, film):
+    def get_images(self, film):
         qs = Image.objects.filter(is_main=True, film=film)
         if qs:
             return qs[0].url
         return "No main image"
-
-    class Meta:
-        model = Film
-        fields = '__all__'
-
-
-class FilmDetailSerializer(serializers.ModelSerializer):
-    images = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='url'
-    )
-    film_type = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    category = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    genres = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
-
-    class Meta:
-        model = Film
-        fields = '__all__'
 
